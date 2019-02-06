10dc9ed5a67e2ccd0cbdc8c275ee6b428c3fd954
William Morgan <billm@wamusa.com>
2019-02-06 00:31:45 -0600

    done

--- main.py
+++ main.py
@@ -29,52 +29,6 @@ def isvalidpost(title,newpost):
         blog_error = 'Please fill in the body'
     return (title_error, blog_error)
 
-@app.route('/edit', methods=['POST', 'GET'])        
-def editpost():
-    if request.method == 'POST':
-        id = int(request.form['id'])
-        title = request.form['title']
-        post_txt = request.form['newpost']
-        post_datetime = request.form['datetime']
-        title_error, blog_error = isvalidpost(title,post_txt)
-        if (title_error or blog_error):
-            return render_template('editpost.html', page_title='Edit a Blog Entry', title_error=title_error, blog_error=blog_error, title=title, newpost=post_txt, id=id)
-        new_blog_post = BlogPost(title, post_txt)
-        new_blog_post.post_datetime = post_datetime
-        db.session.add(new_blog_post)
-        db.session.commit()
-        del_old_post = BlogPost.query.filter_by(id=id).first()
-        db.session.delete(del_old_post)
-        db.session.commit()
-        return redirect('/blog?id={}'.format(new_blog_post.id))
-    id = request.args.get('id')
-    try:
-        id = int(id)
-    except:
-        return redirect('/')
-    blog_post = BlogPost.query.filter_by(id=id).first()
-    if blog_post:
-        title = blog_post.title
-        post_txt = blog_post.post_txt
-        id = blog_post.id
-        datetime = blog_post.post_datetime
-        return render_template('editpost.html', page_title='Edit a Blog Entry', title=title, newpost=post_txt, id=id, datetime=datetime)
-    return redirect('/')
-
-@app.route('/deletepost', methods=['POST', 'GET'])
-def delpost():
-    if request.method == 'POST':
-        if request.form['submit'] != 'Delete':
-            return redirect('/')
-        id = int(request.form['id'])
-        del_post = BlogPost.query.filter_by(id=id).first()
-        db.session.delete(del_post)
-        db.session.commit()
-        return redirect('/')
-    id = int(request.args.get('id'))
-    del_post = BlogPost.query.filter_by(id=id).first()
-    return render_template('deletepost.html', page_title=del_post.title, post=del_post)
-
 @app.route('/newpost', methods=['POST', 'GET'])        
 def newpost():
     error = request.args.get("error")
